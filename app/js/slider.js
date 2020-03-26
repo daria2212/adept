f3aa98cf993c6d794da63e1325eade9479cc7c49
Daria <nabatova.darya@protonmail.com>
2020-03-26 23:44:09 +0300

    some fixes + reviews + contacts + footer

--- app/js/slider.js
+++ app/js/slider.js
@@ -18,4 +18,23 @@ $(function () {
             }
         ]
     });
+
+    $('.reviews__slider').slick({
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    dots: true,
+                    dotsClass: 'menu-dots',
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
